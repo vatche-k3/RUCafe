40f17a3d32dca9a8b5fa6db6c3012f4841103d4e
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-03-24 22:18:43 -0400

    [ ADD ] added Main which is the entry point of app

---
+++ src/cafe/Main.java
@@ -0,0 +1,33 @@
+package cafe;
+
+import cafe.utils.Constants;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+public class Main extends Application {
+
+    /**
+     * Start JavaFX Application
+     * @param primaryStage the primary stage to be shown in the GUI Window
+     * @throws Exception any exceptions that occur
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("./guis/MainMenu.fxml"));
+        primaryStage.setScene(new Scene(root, 500, 600));
+        primaryStage.show();
+        primaryStage.setTitle(Constants.MAIN_MENU_WINDOW_TITLE);
+    }
+
+    /**
+     * Entry point for the entire RUCafe Application
+     * @param args Command line arguments from the JVM
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
