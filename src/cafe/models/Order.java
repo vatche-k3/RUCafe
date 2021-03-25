499a12c60d226d608ea43fa7b8f00fdeeaf11396
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-03-25 15:21:53 -0400

    [ ADD ] converted Orders and StoreOrders
    Both are now singletons

--- src/cafe/models/Order.java
+++ src/cafe/models/Order.java
@@ -1,15 +1,47 @@
 package cafe.models;
 
 
+import java.util.ArrayList;
+
 public class Order implements Customizable {
 
+    ArrayList<MenuItem> itemsInOrder;
+
+    private static Order instance;
+    private Order() {
+        itemsInOrder = new ArrayList<>();
+    }
+    public static Order getInstance() {
+        if(instance == null)
+            instance = new Order();
+        return instance;
+    }
+
+    public void finalizeOrder() {
+        // add to store orders
+        // make a new order
+        StoreOrders.getInstance().add(this);
+        instance = null;
+    }
     @Override
     public boolean add(Object obj) {
+        // TODO
+        // Make sure that obj is a MenuItem
+        // Add to itemsInOrder
+        // Return whether or not it was successful
+        if(obj instanceof MenuItem) {
+            MenuItem objAsMenuItem = (MenuItem)obj;
+            this.itemsInOrder.add(objAsMenuItem);
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean remove(Object obj) {
+        // TODO
+        // Make sure that obj is a MenuItem
+        // Refer to Coffee.remove for a good example
         return false;
     }
 
--- src/cafe/models/StoreOrders.java
+++ src/cafe/models/StoreOrders.java
@@ -1,9 +1,45 @@
 package cafe.models;
 
+import java.util.ArrayList;
+
 public class StoreOrders implements Customizable {
+
+    ArrayList<Order> orders;
+
+    private static StoreOrders instance = null;
+    private StoreOrders() {
+        this.orders = new ArrayList<>();
+    }
+    public static StoreOrders getInstance() {
+        if(instance == null)
+            instance = new StoreOrders();
+        return instance;
+    }
+
+    /**
+     * Export all orders to a file
+     */
+    public void exportOrdersToFile() {
+        //TODO
+        // Open the file dialog, and write orders
+    }
+
+    public void test() {
+        for(Order order : this.orders) {
+            System.out.println("new order");
+            for(MenuItem mItem : order.itemsInOrder) {
+                System.out.println("\t" + mItem.itemPrice());
+            }
+        }
+    }
+
 
     @Override
     public boolean add(Object obj) {
+        if(obj instanceof Order) {
+            Order objAsOrder = (Order)obj;
+            this.orders.add(objAsOrder);
+        }
         return false;
     }
 
