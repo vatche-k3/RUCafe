313e8b3fe4a4d5c81f2103f5df53f8d0f4677275
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-03-31 13:08:43 -0400

    [ FIX / ADD ] added some comments and updated docs

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"cafe.controllers"},{"l":"cafe.models"},{"l":"cafe.utils"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"cafe"},{"l":"cafe.controllers"},{"l":"cafe.models"},{"l":"cafe.utils"}]
--- src/cafe/models/Customizable.java
+++ src/cafe/models/Customizable.java
@@ -1,6 +1,23 @@
 package cafe.models;
 
+/**
+ * Customizable interface. Provides add() and remove() for any object that is customizable, such as:
+ * Coffee, Order, StoreOrder, etc.
+ *
+ * @author Reagan McFarland, Vatche Kafafian
+ */
 public interface Customizable {
+    /**
+     * Add an object
+     * @param obj object to be added
+     * @return status of the operation
+     */
     boolean add(Object obj);
+
+    /**
+     * Remove an object
+     * @param obj object to be removed
+     * @return status of the operation
+     */
     boolean remove(Object obj);
 }
